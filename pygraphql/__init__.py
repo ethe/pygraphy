3d429e963b3497b188df852f5347960f5f1411ad
Gwo <lambda.guo@gmail.com>
2019-07-10 23:30:34 +0800

    Import context easier

--- pygraphql/__init__.py
+++ pygraphql/__init__.py
@@ -1,7 +1,17 @@
-from .types import Interface, Object, Union, Enum, Input, field
+from .types import Interface, Object, Union, Enum, Input, field, context
 from .introspection import Query
 from .view import Schema
 
 
 __version__ = '0.0.1'
-__all__ = ['Interface', 'Object', 'Schema', 'Union', 'Enum', 'Input', 'field', 'Query']
+__all__ = [
+    'Interface',
+    'Object',
+    'Schema',
+    'Union',
+    'Enum',
+    'Input',
+    'field',
+    'Query',
+    'context'
+]
