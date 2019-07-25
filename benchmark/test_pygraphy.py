857cc0b27ad8150d35e9d6caa14127db7afb8b9c
Gwo <lambda.guo@gmail.com>
2019-07-25 10:56:35 +0800

    Add benchmark compare with Strawberry

---
+++ benchmark/test_pygraphy.py
@@ -0,0 +1,42 @@
+import asyncio
+import pygraphy
+from typing import Optional
+
+
+class Patron(pygraphy.Object):
+    id: str
+    name: str
+    age: int
+
+
+class Query(pygraphy.Query):
+
+    @pygraphy.field
+    async def patron(self) -> Patron:
+        await asyncio.sleep(0)
+        return Patron(id='1', name='Syrus', age=27)
+
+
+class Schema(pygraphy.Schema):
+    query: Optional[Query]
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    query = """
+        query something{
+          patron {
+            id
+            name
+            age
+          }
+        }
+    """
+
+    futures = [
+        asyncio.ensure_future(
+            Schema.execute(query), loop=loop
+        ) for _ in range(1000)
+    ]
+    gathered = asyncio.gather(*futures, loop=loop, return_exceptions=True)
+    loop.run_until_complete(gathered)
